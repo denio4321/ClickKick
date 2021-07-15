a4e3ae5a709706d1b929cc33848335b043ecfc90
denio4321 <danimartinezortin@gmail.com>
2021-07-15 13:54:18 +0200

    Instead of showing id once kicking, it will appear it's username.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-from time import sleep
 from g_python.gextension import Extension
 from g_python.hmessage import Direction, HMessage
 from g_python.hpacket import HPacket
@@ -14,12 +13,24 @@ extension_info = {
     "version": "1.0"
 }
 
+def show_them(users):
+    for user in users:
+        user_list.update({user.id: user.name})
+
 ext = Extension(extension_info, args=sys.argv)
 ext.start()
+room_users = RoomUsers(ext)
+room_users.on_new_users(show_them)
+
+user_list = dict()
 
 def kick_user(message: HMessage):
     user_id = message.packet.read_int()
-    ext.send_to_client(HPacket('Whisper', -1, "Kicked user with ID: " + str(user_id), 0, 30, 0, -1))
+    nickname = str()
+    for id in user_list:
+        if id == user_id:
+            nickname = user_list[id]
+    ext.send_to_client(HPacket('Whisper', -1, "Kicked user: " + nickname, 0, 30, 0, -1))
     ext.send_to_server(HPacket('KickUser', user_id))
 
 ext.intercept(Direction.TO_SERVER, kick_user, 'GetSelectedBadges')
